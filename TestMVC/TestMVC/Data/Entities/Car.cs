1e6a34fe2c346e3969e4af326661712321affff9
Mitko6606bg <mitko6606bg@gmail.com>
2024-01-27 11:24:17 +0200

    test MVC

---
+++ TestMVC/TestMVC/Data/Entities/Car.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestMVC.Data.Entities
+{
+    public class Car
+    {
+        [Required]
+        public string Id { get; init; } = Guid.NewGuid().ToString();
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        public string Color { get; set; }
+    }
+}
